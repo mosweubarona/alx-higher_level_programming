370365f9ac4bdf7784115a8be666e55f5c4a4c8e
mosweubaronavb <mosweubarona@gmail.com>
2022-06-21 16:00:05 +0000

    fix 1sqr2nd

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """implimenting"""
+
+
 class Square:
     """Defines module square class"""
+
     def __init__(selt, size):
         """initializing"""
         self._size = size
