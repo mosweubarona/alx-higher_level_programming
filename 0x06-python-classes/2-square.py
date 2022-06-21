84903fc504fd2c91ad2d0b368a584973b033835c
mosweubaronavb <mosweubarona@gmail.com>
2022-06-21 16:07:05 +0000

    fix 2sqr

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """Defines square class"""
+
+
 class Square:
     """Implimentation"""
+
     def __init__(self, size=0):
+        """initiazing"""
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
