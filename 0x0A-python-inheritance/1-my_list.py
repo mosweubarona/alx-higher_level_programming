ea73a685bc9ef8b02ef1d760c9d9a6007c87a73d
mosweubarona <mosweubarona@gmail.com>
2022-07-04 13:00:20 -0700

    my commit 1my

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""
-This module implements a custom list object
+"""Module 1-my_list.
+Creates a class inheriting from the list class.
 """
 
 
 class MyList(list):
-    """Custom List
-    """
+    """Class MyList inherits from list."""
+
     def print_sorted(self):
-        """
-        prints the list, but sorted (ascending sort)
-        """
-        print(sorted(self))
+        """Prints the list, in ascending sort."""
+
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
