e3ddaabdf71a3b82749d14948cf5fbc6a40f5168
mosweubarona <mosweubarona@gmail.com>
2022-07-04 13:33:08 -0700

    my commit fir test 7

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -16,4 +16,6 @@ class Square(Rectangle):
         Args:
             size (int): size
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
