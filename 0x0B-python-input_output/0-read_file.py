205cdfea2dd2303bf605190e847a9a5a9d553a8b
mosweubarona <mosweubarona@gmail.com>
2022-07-05 14:03:51 -0700

    my commit fix indent 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""to read a fill"""
+"""to read a file"""
 
 
 def read_file(filename=""):
     """function to read file"""
     with open(filename, encoding="utf-8") as r:
-            print(r.read(), end="")
+        print(r.read(), end="")
