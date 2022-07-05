f36d153f22e79e9a092b876dd76d8198f082ca63
mosweubarona <mosweubarona@gmail.com>
2022-07-05 13:54:00 -0700

    my commit fix 6

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """function to read file"""
-    with open (filename, encoding="utf-8") as r:
+    with open(filename, encoding="utf-8") as r:
             print(r.read(), end="")
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,4 +7,4 @@ import json
 def load_from_json_file(filename):
     """get file from json"""
     with open(filename, encoding="utf-8") as sv:
-        return json.loa(sv)
+        return json.load(sv)
