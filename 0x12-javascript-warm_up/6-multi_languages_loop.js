10a2b08d743e62046dbb29b4697cceac173b238c
mosweubarona <mosweubarona@gmail.com>
2022-09-05 15:13:45 -0700

    my fix 6 and 8

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const values = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const values = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < values.length; i++) {
   console.log(values[i]);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const square = parseInt(process.argv[2]);
 if (isNaN(square)) {
-  console.log('Missing square');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < square; i++) {
     console.log('X'.repeat(square));
