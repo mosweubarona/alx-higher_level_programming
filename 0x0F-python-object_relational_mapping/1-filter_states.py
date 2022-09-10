ec598e450c32997fd17c8c3353335cdaa2471548
mosweubarona <mosweubarona@gmail.com>
2022-09-10 08:51:44 -0700

    my commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""List all states with a name starting with uppercase N
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+    cur = db.cursor()
+    cmd = """SELECT id, name
+             FROM states
+             WHERE name LIKE BINARY 'N%'
+             ORDER BY id ASC;"""
+    cur.execute(cmd)
+    nStates = cur.fetchall()
+
+    for state in nStates:
+        print(state)
+
+    cur.close()
+    db.close()
