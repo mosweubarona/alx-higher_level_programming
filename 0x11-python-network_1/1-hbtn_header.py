52028beec5834ffc55408547cac258766dbcd78f
mosweubarona <mosweubarona@gmail.com>
2022-09-23 11:11:54 -0700

    m reduce 1 head

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,5 +8,4 @@ if __name__ == "__main__":
     import sys
 
     with urllib.request.urlopen(sys.argv[1]) as response:
-        head = response.headers.get('X-Request-Id')
-        print(head
+        print(response.headers.get('X-Request-Id'))
