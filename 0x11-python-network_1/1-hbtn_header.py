4fd75f062f38b2479bf7c5865c0b55ce2fdb0834
mosweubarona <mosweubarona@gmail.com>
2022-09-23 13:34:41 -0700

    m remove blankline 1hbtn

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,11 +2,10 @@
 # displays the value of the X-Request-Idi
 
 
-import sys
-import urllib.request
-
 if __name__ == "__main__":
+    import sys
+    import urllib.request
 
     req = sys.argv[1]
     with urllib.request.urlopen(req) as response:
-        print(response.headers.get("X-Request-Id"))
+        print(response.headers.get('X-Request-Id'))
