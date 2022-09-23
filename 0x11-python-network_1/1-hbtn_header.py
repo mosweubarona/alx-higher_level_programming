b5b7472f1e6d396e9c1fa80e4b6d17fba5e92d5b
mosweubarona <mosweubarona@gmail.com>
2022-09-23 13:25:25 -0700

    m define req

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,5 +8,5 @@ import urllib.request
 if __name__ == "__main__":
 
     req = sys.argv[1]
-    with urllib.request.urlopen(request) as response:
+    with urllib.request.urlopen(req) as response:
         print(response.headers.get("X-Request-Id"))
