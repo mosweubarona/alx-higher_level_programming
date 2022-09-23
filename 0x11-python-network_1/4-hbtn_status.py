e6be0c4e22f596dd620b7edc3ba298c5733a4372
mosweubarona <mosweubarona@gmail.com>
2022-09-23 11:05:39 -0700

    m checker 7 4ststus

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status
 """
-import requests
+
 
 if __name__ == "__main__":
+    import requests
+
     url = "https://intranet.hbtn.io/status"
     X = requests.get(url)
+
     print("Body response:")
     print("\t- type: {}".format(type(X.text)))
     print("\t- content: {}".format(X.text))
