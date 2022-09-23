8c6e844c3b527b7d2e276fa45158bba909711f21
mosweubarona <mosweubarona@gmail.com>
2022-09-23 10:06:59 -0700

    my rearenge import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # displays the value of the X-Request-Id
-import urllib.request
-import sys
 # prevent code to be executed
 if __name__ == "__main__":
+    import urllib.request
+    import sys
     with urllib.request.urlopen(sys.argv[1]) as reply:
         response = reply.headers.get('X-Request-Id')
         print(response)
--- 0x11-python-network_1/README.md
+++ 0x11-python-network_1/README.md
@@ -1,1 +1,2 @@
-## more on networking
+[main 4388730] my commit
+ 1 file changed, 3 insertions(+), 2 deletions(-)
