86c187f5a515cd4bd2a1b8c2501efd9bd3660382
mosweubarona <mosweubarona@gmail.com>
2022-09-23 08:58:47 -0700

    my commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#fatchs the url
+import urllib.request
+#open connection to url
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
